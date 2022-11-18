dff8977b12e1f992fe1ac1a0a3c3ac225e76a2c5
SimoGhilo <simoneghilotti@gmail.com>
2022-11-18 09:37:11 +0000

    18.11.22

--- controller.js
+++ controller.js
@@ -10,14 +10,52 @@ const getCustomers = (req, res) => {
 
 const getCustomerById = (req, res) => {
     const id = parseInt(req.params.id);
-    pool.query(`select * from students where id=${(id).toString()}`, (err, result) => {
+    pool.query(`select * from customers where id=${id.toString()}`, (err, result) => {
         if (err) throw err;
         res.status(200).json(result.rows);
     })
 }
 
 
+const deleteCustomerById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query(`select * from customers where id=${id.toString()}`, (err, result) => {
+        const notFound = !result.rows.length;
+        if (notFound) {
+            res.send('Customer does not exist in the database');
+        }
+
+        pool.query(`delete from customers where id=${id.toString()}`, (err, result) => {
+            if (err) throw err;
+            res.status(200).send('Student removed successfully');
+        });
+    })
+}
+
+const updateCustomerById = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+    const { address } = req.body;
+    const { email } = req.body;
+
+    pool.query(`select * from customers where id=${id.toString()}`, (err, result) => {
+        const notFound = !result.rows.length;
+        if (notFound) {
+            res.send('Customer does not exist in the database');
+        }
+
+
+
+        pool.query(`update customers set name=${name}, address=${address},email=${email} where id=${id}}`,
+            (err, result) => {
+                if (err) throw err;
+                res.status(200).send('Student updated successfully');
+            });
+    })
+}
+
 
 module.exports = {
-    getCustomers, getCustomerById
+    getCustomers, getCustomerById, deleteCustomerById, updateCustomerById
 }
--- routes/customer.js
+++ routes/customer.js
@@ -4,26 +4,6 @@ const express = require('express');
 const customerRouter = express.Router();
 const controller = require('../controller');
 
-
-
-/*const customers = [
-    {
-        name: 'John',
-        id: 1,
-        age: 30
-    },
-    {
-        name: 'Mark',
-        id: 2,
-        age: 10
-    },
-    {
-        name: 'Andrew',
-        id: 3,
-        age: 60
-    }
-] */
-
 // get all customers - Test
 customerRouter.get('/',
     controller.getCustomers)
@@ -31,22 +11,10 @@ customerRouter.get('/',
 /// get a customer by id 
 customerRouter.get('/:id', controller.getCustomerById);
 
-customerRouter.delete('/:id', controller.getCustomerById);
+// delete a customer by id
+customerRouter.delete('/:id', controller.deleteCustomerById);
 
-// update a  customer   / Not working properly
-// look up the customer, if not found return 404, else update the customer and return the updated customer
-/*customerRouter.put('/:id', (req, res) => {
-
-    const customer = customers.find(c => c.id === parseInt(req.params.id));
-    if (!customer) {
-        res.status(404).res.send(`The customer with id ${req.params.id} was not found`);
-    } else {
-
-        customer.name = req.body.name;
-        customer.age = req.body.age;
-        res.send(customer);
-    }
-
-}) */
+// update a  customer by id
+customerRouter.put('/:id', controller.updateCustomerById);
 
 module.exports = customerRouter;
